c52a72377b570e924446af602e73d525ea066e34
omnom-nomi <morisnaomi13@gmail.com>
2024-05-18 16:49:39 +0100

    added a footer and food component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />
+      <Food />
+    </>
   );
-  
+
 }
 
 export default App
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,7 +1,15 @@
-function Header(){
-    return(
+function Header() {
+    return (
         <header>
             <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
         </header>
 
     );
