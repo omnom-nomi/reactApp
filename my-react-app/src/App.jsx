e0dbaa84b77fa35e825853b4c2775919637bc993
omnom-nomi <morisnaomi13@gmail.com>
2024-05-18 19:08:33 +0100

    removed previous files and replaced them with a card component. Also added some css styles

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header"
-import Footer from "./Footer";
-import Food from "./Food";
-
+import Card from "./Card";
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
-      <Food />
+      <Card/>
+      <Card/>
     </>
   );
 
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+    return (
+        <div className="card">
+
+            <img className="card-image" src="https://picsum.photos/200/300" alt="random photo" />
+            <h2 className="card-title" >Random Image</h2>
+            <p className="card-text" >I am a student and listen to K-pop</p>
+
+        </div>
+
+    )
+
+}
+
+export default Card;
